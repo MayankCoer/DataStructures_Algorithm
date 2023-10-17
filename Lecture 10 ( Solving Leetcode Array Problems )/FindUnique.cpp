78a7b4b6643667f99c387238c6d83fdce235bb61
MayankCoer <mayankpal9088@gmail.com>
2023-10-18 03:34:04 +0530

    New Lectures added

---
+++ Lecture 10 ( Solving Leetcode Array Problems )/FindUnique.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void findUnique(int arr[], int size)
+{
+    int ans = 0 ;
+    for(int i = 0 ; i < size ; i++){
+        ans = ans ^ arr[i] ;
+    }
+    cout << ans << endl;
+}
+int main(){
+    int arr[5] = {2 , 4 ,7 ,2 ,7};
+    findUnique(arr,5);
+}
