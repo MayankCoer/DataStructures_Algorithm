c756c09413856538c1b807b33adbc4d675f8c885
MayankPal <mayankpal9088@gmail.com>
2023-10-25 20:29:36 +0530

    NewFile Added

--- Lecture 10 ( Solving Leetcode Array Problems )/FindAllDuplicatesInArray.cpp
+++ Lecture 10 ( Solving Leetcode Array Problems )/FindAllDuplicatesInArray.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> array;
-        for(int i = 0 ; i < nums.size() ; i++){
-            for(int j = i+1 ; j < nums.size() ; j++){
-                if((nums[i] ^ nums[j]) == 0){
-                    array.push_back(nums[i]);
-                }
-                else{
-                    continue;
-                }
+        int i = 0;
+        int j = i+1;
+        sort(nums.begin() , nums.end());
+        
+        for(int i = 0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]){
+                array.push_back(nums[i]);
+            }
+            else{
+                continue;
             }
         }
         return array;
